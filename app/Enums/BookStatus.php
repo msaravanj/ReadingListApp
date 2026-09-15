4c81201665c9a97f3753c5b69e568a2dbccc5538
msaravanj <sarosaravanja11@gmail.com>
2026-09-15 22:23:47 +0200

    Dodane migracije za tablice Books, Categories te pivot tablicu Book_Category

---
+++ app/Enums/BookStatus.php
@@ -0,0 +1,7 @@
+enum BookStatus: string
+
+{
+    case WANT_TO_READ = 'want_to_read';
+    case READING = 'reading';
+    case READ = 'read';
+}
---
+++ database/migrations/2026_09_14_212159_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('status');
+            $table->integer('rating')
+                ->nullable()
+                ->comment('0-5')
+                ->check('rating >= 0 AND rating <= 5');
+            $table->text('notes')
+                ->nullable();
+            $table->string('cover_image')
+                ->nullable();
+            $table->string('isbn')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
